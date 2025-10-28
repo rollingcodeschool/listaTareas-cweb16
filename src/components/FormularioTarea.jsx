6bd30fae486af7420bd427697a4f297df5627927
Emilse Arias <emilsearias@gmail.com>
2025-10-28 11:22:22 -0300

    creamos tareas y las agregamos al state arrayTareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,14 +1,26 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+const [arrayTareas, setArrayTareas] = useState([])
+const [tarea, setTarea] = useState('')
+
+const handleSubmit = (e)=>{
+    e.preventDefault();
+    //tomar el valor del state tarea y almacenarlo en el arrayTareas
+    setArrayTareas([...arrayTareas, tarea])
+    //limpiar el state tarea
+    setTarea('')
+}
+
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <div className="d-flex">
-            <Form.Control type="text" placeholder="Ingresa una tarea" />
+            <Form.Control type="text" placeholder="Ingresa una tarea" onChange={(e)=> setTarea(e.target.value)} value={tarea}/>
             <Button variant="primary" type="submit">
               Enviar
             </Button>
