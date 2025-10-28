22ac774f0921255bd658f3c74c9b018599d7584f
Emilse Arias <emilsearias@gmail.com>
2025-10-28 10:05:22 -0300

    agregamos un color de fondo al root

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1>hola mundo</h1>
+    <main className="container py-5">
+      <h1 className="text-center text-light">Bienvenidos</h1>
+      {/* aqui agrego el resto de los componentes */}
+
+    </main>
     </>
   );
 }
