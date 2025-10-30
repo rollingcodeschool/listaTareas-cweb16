155fee0e7e90a7010d203a1bb4b388f0a181baea
Emilse Arias <emilsearias@gmail.com>
2025-10-30 11:34:16 -0300

    agregamos una imagen como link y una imagen importando un archivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import FormularioTarea from "./components/FormularioTarea";
-
+import imagen from './img/paisaje.jpeg'
 function App() {
   return (
     <>
     <main className="container py-5">
+      <img src="https://images.pexels.com/photos/34350110/pexels-photo-34350110.jpeg" alt="paisaje" className="img-fluid" />
+      <img src={imagen} alt="paisaje 2" className="img-fluid" />
       <h1 className="text-center text-light">Bienvenidos</h1>
       {/* aqui agrego el resto de los componentes */}
       <FormularioTarea></FormularioTarea>
